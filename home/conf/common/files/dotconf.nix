ee44f70c5445568eaa4aec5ea501074e022c3150
David Reis <git@d-reis.com>
2025-04-23 15:40:41 +0200

    Add mdformat config

--- home/conf/common/files/dotconf.nix
+++ home/conf/common/files/dotconf.nix
@@ -4,4 +4,8 @@
   ".local/gitbin".source = "${inputs.dotbin}";
   ".local/include".source = "${inputs.dotinc}";
   ".inputrc".source = "${inputs.dotconf}/.inputrc";
+  ".mdformat.toml".text = ''
+    wrap=80
+    extensions=["gfm"]
+  '';
 }
