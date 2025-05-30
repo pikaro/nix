106f494e0647053088358f938ee5b98129112f16
David Reis <git@d-reis.com>
2025-05-30 12:25:48 +0200

    Install various brew casks

--- pkg/brew/casks/common/apps.nix
+++ pkg/brew/casks/common/apps.nix
@@ -1,12 +1,17 @@
 [
+  "google-chrome"
+  "google-chrome@canary"
   "discord"
   "docker"
+  "firefox"
   "joplin"
   "localsend"
   "kitty"
+  "karabiner-elements"
   "nuage"
   "openmtp"
   "slack"
   "spotify"
   "unnaturalscrollwheels"
+  "wireshark"
 ]
