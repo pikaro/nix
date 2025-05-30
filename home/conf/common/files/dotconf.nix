21313e7db4ee077e9369209ff582f35f4922cfe8
David Reis <git@d-reis.com>
2025-05-30 12:26:22 +0200

    Install Karabiner

--- home/conf/common/files/dotconf.nix
+++ home/conf/common/files/dotconf.nix
@@ -4,6 +4,7 @@
   ".local/gitbin".source = "${inputs.dotbin}";
   ".local/include".source = "${inputs.dotinc}";
   ".inputrc".source = "${inputs.dotconf}/.inputrc";
+  ".config/karabiner/karabiner.json".source = "${inputs.dotconf}/karabiner.json";
   ".mdformat.toml".text = ''
     wrap=80
     extensions=["gfm"]
