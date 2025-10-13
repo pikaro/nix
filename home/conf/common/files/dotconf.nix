d42fb5d336f10a2668735e3abb0f0382705a9726
David Reis <git@d-reis.com>
2025-10-13 19:33:02 +0200

    feat: Add aider

--- home/conf/common/files/dotconf.nix
+++ home/conf/common/files/dotconf.nix
@@ -5,6 +5,19 @@
   ".local/include".source = "${inputs.dotinc}";
   ".inputrc".source = "${inputs.dotconf}/.inputrc";
 
+  ".aider.model.settings.yml".text = ''
+    - name: aider/extra_params
+      extra_params:
+        extra_headers:
+          Editor-Version: vscode/42
+  '';
+  ".aider.conf.yml".text = ''
+    model: github_copilot/gpt-5
+    editor-model: github_copilot/gpt-5
+    weak-model: github_copilot/gpt-5-mini
+    show-model-warnings: false
+  '';
+
   ".config/karabiner/karabiner.json".source = "${inputs.dotconf}/karabiner.json";
   ".mdformat.toml".text = ''
     wrap=80
--- pkg/nix/common/apps.nix
+++ pkg/nix/common/apps.nix
@@ -1,5 +1,6 @@
 {pkgs}:
 with pkgs; [
+  aider-chat
   dbeaver-bin
   # FIXME: Only creates link, messes with Dock, how to fix?
   #kitty
