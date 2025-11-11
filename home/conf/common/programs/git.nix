cc5e8cf80cbe8905a7d891625729e9b6b2a41dd9
David Reis <git@d-reis.com>
2025-11-11 15:20:02 +0100

    fix: git config split

--- home/conf/common/programs/git.nix
+++ home/conf/common/programs/git.nix
@@ -1,41 +1,44 @@
 {sysInfo, ...}: {
   enable = true;
-  aliases = {
-    "a" = "add";
-    "aa" = "add .";
-    "b" = "!git for-each-ref --color=always --sort='-committerdate:iso8601' --format=' %(color:brightblack)%(committerdate:iso8601)%09%(committerdate:relative)%09%(color:blue)[%(authorname)]%09%(if:equals=master)%(refname:short)%(then)%(color:red)%(else)%(if:equals=development)%(refname:short)%(then)%(color:green)%(else)%(color:cyan)%(end)%(end)%(refname:short)%09%(color:reset)%(contents:subject)' refs/heads | column -ts$'\\t'";
-    "c" = "checkout";
-    "ca" = "commit --amend";
-    "can" = "commit --amend --no-edit --no-verify";
-    "cm" = "commit -m";
-    "cmn" = "commit --no-verify -m";
-    "d" = "diff -w";
-    "f" = "fetch";
-    "l" = "log --pretty=format:\"%C(yellow)%h\\ %ai%Cred%d\\ %C(brightwhite)%s%Cblue\\ [%cn]\" --decorate --date=short";
-    "lg" = "!git log --graph --oneline --decorate --all";
-    "m" = "merge";
-    "p" = "push";
-    "u" = "pull";
-    "s" = "status";
-    "t" = "tag --sort=-taggerdate";
-    "ff" = "flow feature";
-    "ffc" = "flow feature checkout";
-    "ffs" = "flow feature start";
-    "fff" = "flow feature finish --squash --force_delete";
-    "ffp" = "flow feature publish";
-    "fr" = "flow release";
-    "frc" = "flow release checkout";
-    "frs" = "flow release start";
-    "frf" = "flow release finish --keepremote";
-    "frp" = "flow release publish";
-    "fh" = "flow hotfix";
-    "fhc" = "flow hotfix checkout";
-    "fhs" = "flow hotfix start";
-    "fhf" = "flow hotfix finish --squash --force_delete";
-    "fhp" = "flow hotfix publish";
-  };
-  delta.enable = true;
-  extraConfig = {
+  settings = {
+    alias = {
+      "a" = "add";
+      "aa" = "add .";
+      "b" = "!git for-each-ref --color=always --sort='-committerdate:iso8601' --format=' %(color:brightblack)%(committerdate:iso8601)%09%(committerdate:relative)%09%(color:blue)[%(authorname)]%09%(if:equals=master)%(refname:short)%(then)%(color:red)%(else)%(if:equals=development)%(refname:short)%(then)%(color:green)%(else)%(color:cyan)%(end)%(end)%(refname:short)%09%(color:reset)%(contents:subject)' refs/heads | column -ts$'\\t'";
+      "c" = "checkout";
+      "ca" = "commit --amend";
+      "can" = "commit --amend --no-edit --no-verify";
+      "cm" = "commit -m";
+      "cmn" = "commit --no-verify -m";
+      "d" = "diff -w";
+      "f" = "fetch";
+      "l" = "log --pretty=format:\"%C(yellow)%h\\ %ai%Cred%d\\ %C(brightwhite)%s%Cblue\\ [%cn]\" --decorate --date=short";
+      "lg" = "!git log --graph --oneline --decorate --all";
+      "m" = "merge";
+      "p" = "push";
+      "u" = "pull";
+      "s" = "status";
+      "t" = "tag --sort=-taggerdate";
+      "ff" = "flow feature";
+      "ffc" = "flow feature checkout";
+      "ffs" = "flow feature start";
+      "fff" = "flow feature finish --squash --force_delete";
+      "ffp" = "flow feature publish";
+      "fr" = "flow release";
+      "frc" = "flow release checkout";
+      "frs" = "flow release start";
+      "frf" = "flow release finish --keepremote";
+      "frp" = "flow release publish";
+      "fh" = "flow hotfix";
+      "fhc" = "flow hotfix checkout";
+      "fhs" = "flow hotfix start";
+      "fhf" = "flow hotfix finish --squash --force_delete";
+      "fhp" = "flow hotfix publish";
+    };
+    user = {
+      email = "git@d-reis.com";
+      name = sysInfo.userName;
+    };
     init = {
       defaultBranch = "dev";
       templateDir = "~/.git-template";
@@ -80,6 +83,4 @@
   lfs = {
     enable = true;
   };
-  userEmail = "git@d-reis.com";
-  userName = sysInfo.userName;
 }
