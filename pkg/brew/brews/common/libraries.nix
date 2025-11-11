09a588d08bf263e949c0631df5a87344954de29c
David Reis <git@d-reis.com>
2025-11-11 15:20:50 +0100

    docs: Add QEMU deps

---
+++ pkg/brew/brews/common/libraries.nix
@@ -0,0 +1,19 @@
+[
+  # Dependencies of ESPIDF QEMU
+  # Failed to install via nix-shell due to
+  # hard dep on MacOS SDK 15.4
+  # "bzip2"
+  # "capstone"
+  # "glib"
+  # "libgcrypt"
+  # "libpng"
+  # "libusb"
+  # "lzo"
+  # "pcre2"
+  # "pkgconf"
+  # "snappy"
+  # "zstd"
+  # "gtk+3"
+  # "pixman"
+  # "sdl2"
+]
