16adef448bc4e25c398691edd30da191a7d3e8b1
David Reis <git@d-reis.com>
2025-11-29 18:59:15 +0100

    feat: Add tkinter

--- pkg/nix/common/languages.nix
+++ pkg/nix/common/languages.nix
@@ -7,19 +7,22 @@ with pkgs; [
   pnpm
   pkgconf
   poetry
+  # WARNING: python314 is broken
   (python313.withPackages
     (ps:
       with ps; [
         boto3
+        customtkinter
         gitpython
-        # FIXME: BROKEN
-        # datamodel-code-generator
+        datamodel-code-generator
         mdformat
         mdformat-gfm
         mdformat-mkdocs
         mdformat-frontmatter
         pip
+        pyobjc-framework-Cocoa
         tabulate
+        tkinter
       ]))
   rustc
   uv
