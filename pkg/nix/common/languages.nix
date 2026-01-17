8071c1db5a3bfbcef2e2826614159e72abcec889
David Reis <git@d-reis.com>
2026-01-17 22:38:06 +0100

    fix: Disable broken poetry

--- pkg/nix/common/languages.nix
+++ pkg/nix/common/languages.nix
@@ -15,24 +15,29 @@ with pkgs; [
   nodejs
   pnpm
   pkgconf
-  poetry
+  # FIXME: BROKEN
+  # poetry
   # WARNING: python314 is broken
-  (python313.withPackages
-    (ps:
-      with ps; [
-        boto3
-        customtkinter
-        gitpython
-        datamodel-code-generator
-        mdformat
-        mdformat-gfm
-        mdformat-mkdocs
-        mdformat-frontmatter
-        pip
-        pyobjc-framework-Cocoa
-        tabulate
-        tkinter
-      ]))
+  (
+    python313.withPackages
+    (
+      ps:
+        with ps; [
+          boto3
+          customtkinter
+          gitpython
+          datamodel-code-generator
+          mdformat
+          mdformat-gfm
+          mdformat-mkdocs
+          mdformat-frontmatter
+          pip
+          pyobjc-framework-Cocoa
+          tabulate
+          tkinter
+        ]
+    )
+  )
   rustc
   uv
   yarn
