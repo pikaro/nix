9c4a0ee7d5c2e945545e19dc8cdb0f9cac114aa0
David Reis <git@d-reis.com>
2026-01-24 14:11:42 +0100

    feat: Add custom flake only updating

--- apply.sh
+++ apply.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
+SYSTEM_FLAKES=(
+    "root"
+    "nixpkgs"
+    "nix-darwin"
+    "home-manager"
+)
+
 if [ "${1:-}" = "-u" ]; then
     nix flake update
+    shift
+fi
+
+if [ "${1:-}" = "-U" ]; then
+    SYSTEM_FLAKES_JSON="$(printf '%s\n' "${SYSTEM_FLAKES[@]}" | jq -R . | jq -s .)"
+    echo "${SYSTEM_FLAKES_JSON}" >&2
+    nix flake metadata --json |
+        jq -r --argjson system "${SYSTEM_FLAKES_JSON}" '(.locks.nodes | keys - $system)[]' |
+        xargs nix flake update
     shift
 fi
 
