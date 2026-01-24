cdb81de5e8ca06970086212c1443a62e38dd1e66
David Reis <git@d-reis.com>
2026-01-24 14:42:48 +0100

    feat: Add logging

--- apply.sh
+++ apply.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eEuo pipefail
+
 SYSTEM_FLAKES=(
     "root"
     "nixpkgs"
@@ -7,26 +9,55 @@ SYSTEM_FLAKES=(
     "home-manager"
 )
 
+_log() {
+    if [[ ! -t 0 ]]; then
+        MSG="$1: $(cat /dev/stdin)"
+    else
+        MSG="$1"
+    fi
+    printf "[%s] %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "$MSG" >/dev/stderr
+}
+
+_log "Updating nix channels"
+
 if [ "${1:-}" = "-u" ]; then
+    _log "Updating all flakes"
     nix flake update
     shift
 fi
 
 if [ "${1:-}" = "-U" ]; then
     SYSTEM_FLAKES_JSON="$(printf '%s\n' "${SYSTEM_FLAKES[@]}" | jq -R . | jq -s .)"
-    echo "${SYSTEM_FLAKES_JSON}" >&2
     nix flake metadata --json |
-        jq -r --argjson system "${SYSTEM_FLAKES_JSON}" '(.locks.nodes | keys - $system)[]' |
+        jq -r --argjson system "${SYSTEM_FLAKES_JSON}" '(.locks.nodes | keys - $system)[]' | tr '\n' ' ' |
+        tee >(_log "Updating flakes") |
         xargs nix flake update
     shift
 fi
 
-git add flake.lock
+CLEANUP=0
+if [ "${1:-}" = "-c" ]; then
+    CLEANUP=1
+    shift
+fi
+
+if git diff --quiet flake.lock; then
+    _log "No changes in flake.lock"
+else
+    _log "flake.lock has changed, committing the changes"
+    git commit -am "Update flake.lock"
+fi
+
+_log "Rebuilding nix-darwin configuration"
 sudo darwin-rebuild switch --flake . "$@"
 
-if [ "${1:-}" = "-c" ]; then
+if [ "${CLEANUP}" -eq 1 ]; then
+    _log "Cleaning up brew"
+    brew cleanup --prune=all
+    _log "Cleaning up old nix generations"
     nix-env --delete-generations old
-    nix-store gc --gc
+    _log "Collecting nix garbage"
     nix-collect-garbage -d
+    _log "Optimising nix store"
     nix-store --optimise
 fi
